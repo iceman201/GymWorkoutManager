9079fd01dd4b6528041bcc2a028f673934b34fbf
Liguo Jiao <liguo.jiao@mckay.co.nz>
2016-01-18 12:54:39 +1300

    Re-Create project

---
+++ GymWorkoutManager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GymWorkoutManager
+//
+//  Created by Liguo Jiao on 18/01/16.
+//  Copyright © 2016 McKay. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
