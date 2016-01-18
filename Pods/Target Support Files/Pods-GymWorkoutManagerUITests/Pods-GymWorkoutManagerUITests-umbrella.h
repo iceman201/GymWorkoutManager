b2342c06ced5f4e440552851600382440196ceba
Liguo Jiao <liguo.jiao@mckay.co.nz>
2016-01-18 13:10:19 +1300

    Create cocoaPods & install realm

---
+++ Pods/Target Support Files/Pods-GymWorkoutManagerUITests/Pods-GymWorkoutManagerUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GymWorkoutManagerUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GymWorkoutManagerUITestsVersionString[];
+
