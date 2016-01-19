b6136fa1feef9bb5af5b8a00115fc45d4f97bf87
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2016-01-19 21:10:20 +1300

    Done, and fix app icon

--- GymWorkoutManager/RecordViewController.swift
+++ GymWorkoutManager/RecordViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class RecordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- GymWorkoutManager/TimerViewController.swift
+++ GymWorkoutManager/TimerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, TimeSetupViewControllerDelegate {
+class TimerViewController: UIViewController, TimeSetupViewControllerDelegate {
     
     // MARK: - IBOutlets
     
