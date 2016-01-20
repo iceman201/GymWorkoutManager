b79bf587caf9c7c86bf4b99cfc076aca06d4a4b2
Liguo Jiao <liguo.jiao@mckay.co.nz>
2016-01-20 16:13:30 +1300

    Finished countdown and running counting

--- GymWorkoutManager/FirstViewController.swift
+++ GymWorkoutManager/FirstViewController.swift
@@ -16,32 +16,55 @@ class FirstViewController: UIViewController, TimeSetupViewControllerDelegate {
     @IBOutlet var totalWorkoutTimer: UILabel!
     @IBOutlet var aroundNumber: UILabel!
     @IBOutlet var startButton: UIButton!
-    
-    @IBAction func counter(sender: AnyObject) {
-        NSTimer.scheduledTimerWithTimeInterval(0.001, target: self, selector: Selector("timeCount"), userInfo: nil, repeats: true)
-    }
-    
-    func timeCount() {
-        self.time = self.time.dateByAddingTimeInterval(-0.001)
-        self.repeatTimer.text = self.timeString(self.time)
-        NSLog("time: %@", self.repeatTimer.text!)
-    }
-    
     // MARK: - Variables
     var receivedTime : [String] = []
     var time: NSDate = NSDate()
+    
+    var startRuningTime = NSTimeInterval()
+    var timerRuning = NSTimer()
+    var timerCountdown = NSTimer()
+
+    
+    @IBAction func counter(sender: AnyObject) {
+        if startButton.currentTitle != "Stop" {
+            startButton.setTitle("Stop", forState: .Normal)
+            timerCountdown = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("timeCountdown"), userInfo: nil, repeats: true)
+            timerRuning = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("timeRuning"), userInfo: nil, repeats: true)
+            startRuningTime = NSDate.timeIntervalSinceReferenceDate()
+        } else {
+            startButton.setTitle("GO!", forState: .Normal)
+            timerCountdown.invalidate()
+            timerRuning.invalidate()
+        }
+        
+    }
+    
+    func timeRuning() {
+        let currentTime = NSDate.timeIntervalSinceReferenceDate()
+        var elapsedTime: NSTimeInterval = currentTime - startRuningTime
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        let fraction = UInt8(elapsedTime * 100)
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        self.totalWorkoutTimer.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+    }
+    
+    func timeCountdown() {
+        self.time = self.time.dateByAddingTimeInterval(-0.01)
+        self.repeatTimer.text = self.timeString(self.time).stringByReplacingOccurrencesOfString(".", withString: ":")
+    }
     
     // MARK: - View
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -52,27 +75,27 @@ class FirstViewController: UIViewController, TimeSetupViewControllerDelegate {
     func timeSetupFinish(timeSetupViewController: TimeSetupViewController, result: [String]) {
         self.receivedTime = result
         self.time = self.timeDate(result)
-        self.repeatTimer.text = self.timeString(result)
+        self.repeatTimer.text = self.timeString(result).stringByReplacingOccurrencesOfString(".", withString: ":")
         self.aroundNumber.text = result[2]
     }
     
     // MARK: - Private Method
     private func timeDate(time: [String]) -> NSDate {
         let numberFormatter = NSNumberFormatter();
-        let repeatString = String.init(format: "%02d:%02d.000", numberFormatter.numberFromString(time[0])!.longValue, numberFormatter.numberFromString(time[1])!.longValue)
+        let repeatString = String.init(format: "%02d:%02d.00", numberFormatter.numberFromString(time[0])!.longValue, numberFormatter.numberFromString(time[1])!.longValue)
         let dateFormatter = NSDateFormatter();
-        dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("mm:ss.SSS", options: 0, locale: NSLocale.currentLocale())
+        dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("mm:ss.SS", options: 0, locale: NSLocale.currentLocale())
         return dateFormatter.dateFromString(repeatString)!
     }
 
     private func timeString(time: [String]) -> String {
         let numberFormatter = NSNumberFormatter();
-        return String.init(format: "%02d:%02d.000", numberFormatter.numberFromString(time[0])!.longValue, numberFormatter.numberFromString(time[1])!.longValue)
+        return String.init(format: "%02d:%02d.00", numberFormatter.numberFromString(time[0])!.longValue, numberFormatter.numberFromString(time[1])!.longValue)
     }
     
     private func timeString(timeDate: NSDate) -> String {
         let dateFormatter = NSDateFormatter();
-        dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("mm:ss.SSS", options: 0, locale: NSLocale.currentLocale())
+        dateFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("mm:ss.SS", options: 0, locale: NSLocale.currentLocale())
         return dateFormatter.stringFromDate(timeDate)
     }
     
