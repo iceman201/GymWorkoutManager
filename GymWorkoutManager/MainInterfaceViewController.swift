fadc56009da47efbe124379e39f6c48a74186d77
Liguo Jiao <liguo.jiao@mckay.co.nz>
2016-01-25 14:33:02 +1300

    Done

---
+++ GymWorkoutManager/MainInterfaceViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainInterfaceViewController.swift
+//  GymWorkoutManager
+//
+//  Created by Liguo Jiao on 25/01/16.
+//  Copyright © 2016 McKay. All rights reserved.
+//
+
+import UIKit
+
+class MainInterfaceViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
