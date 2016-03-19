67abaa6ecf04698cc65a809900c5820b22391098
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2016-03-19 16:33:13 +1300

    Record-Function
    - add Harris Benedict Method formular

--- GymWorkoutManager/PersonalInformation.swift
+++ GymWorkoutManager/PersonalInformation.swift
@@ -10,7 +10,7 @@ import UIKit
 import ChameleonFramework
 import JVFloatLabeledTextField
 
-class PersonalInformation: UIViewController {
+class PersonalInformation: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var name: JVFloatLabeledTextField!
     @IBOutlet var gender: UISegmentedControl!
@@ -21,22 +21,44 @@ class PersonalInformation: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        name.delegate = self
+        age.delegate = self
+        bodyFat.delegate = self
+        weight.delegate = self
+        height.delegate = self
+        print(gender.selectedSegmentIndex)
     }
     
+    @IBAction func selectGender(sender: AnyObject) {
+        if gender.selectedSegmentIndex == 0 {
+            print("M")
+        } else if gender.selectedSegmentIndex == 1 {
+            print("F")
+        }
+    }
     
-    private func BMRCalculation1(age:Int, weight:Float, height:Float, gender:String) {
-        //Katch & McArdle Method
-        /*BMR Men: = 66 + (6.23 X weight in pounds) + (12.7 X height in inches) – (6.8 X age)
+    private func BMRCalculation1(a:Int, w:Float, h:Float, gender:Int) -> Float{
+        //Harris Benedict Method
+        /*BMR Men: BMR = 66.5 + ( 13.75 x weight in kg ) + ( 5.003 x height in cm ) – ( 6.755 x age in years )
         
-        BMR Women: = 655 + (4.35 X weight in pounds) + (4.7 X height in inches) – (4.7 X age)*/
-        
+        BMR Women: BMR = 655.1 + ( 9.563 x weight in kg ) + ( 1.850 x height in cm ) – ( 4.676 x age in years )
+        */
+        var result : Float = 0.0
+        if gender == 0 { // 0 For male
+            result = 66+(13.75*w)+(5.003*h)-(6.755 * Float(a))
+        } else if gender == 1 { // 1 For female
+            result = 655.1+(9.563*w)+(1.85*h)-(4.676 * Float(a))
+        }
+        return result
     }
     private func BMRCalculation2(age:Int, weight:Float, height:Float, gender:String, bodyFat:String) {
         //Katch & McArdle Method
-        /*BMR (Men + Women) = 370 + (21.6 * Lean Mass in kg)
+        /*
+        BMR (Men + Women) = 370 + (21.6 * Lean Mass in kg)
+        
         Lean Mass = weight in kg – (weight in kg * body fat %)
-        1 kg = 2.2 pounds, so divide your weight by 2.2 to get your weight in kg*/
+        1 kg = 2.2 pounds, so divide your weight by 2.2 to get your weight in kg
+        */
     }
 
 }
