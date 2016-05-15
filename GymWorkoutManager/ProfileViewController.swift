17eb5159f034d90428178f29b518da74dae364f7
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2016-05-15 23:16:24 +1200

    code tidy up

--- GymWorkoutManager/ProfileViewController.swift
+++ GymWorkoutManager/ProfileViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import RealmSwift
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    
+    // MARK: - IBOutlet
     @IBOutlet var profilePicture: RoundButton!
     @IBOutlet var headerView: UIView!
+    // MARK: - Variables
     var curentUser:Person?
     
+    // MARK: Profile Image
     @IBAction func selectPicture(sender: AnyObject) {
         let alert = UIAlertController(title: "Profile image", message: "Upload your profile image.", preferredStyle: .ActionSheet)
         alert.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { (UIAlertAction) in
@@ -35,6 +36,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
     }
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         let i = info[UIImagePickerControllerOriginalImage] as? UIImage
         let image = fixOrientation(i!)
@@ -58,20 +60,57 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    func fixOrientation(img:UIImage) -> UIImage {
-        
-        if (img.imageOrientation == UIImageOrientation.Up) {
-            return img;
+    
+    private func fixOrientation(image:UIImage) -> UIImage {
+        if (image.imageOrientation == UIImageOrientation.Up) {
+            return image
         }
-        UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale);
-        let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
-        img.drawInRect(rect)
-        
+        UIGraphicsBeginImageContextWithOptions(image.size, false, image.scale)
+        let rect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+        image.drawInRect(rect)
         let normalizedImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext();
-        return normalizedImage;
-        
+        UIGraphicsEndImageContext()
+        return normalizedImage
     }
+    
+    private func resizeToAspectFit(viewSize: CGSize, bounds: CGRect, sourceImage: UIImage) -> UIImage {
+        UIGraphicsBeginImageContext(viewSize)
+        sourceImage.drawInRect(bounds)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    private func profilePictureStyleSheet() {
+        profilePicture.layer.borderWidth = 3
+        profilePicture.backgroundColor = GWMColorPurple
+        profilePicture.layer.shadowColor = UIColor.blackColor().CGColor
+        profilePicture.layer.shadowOffset = CGSize(width: 0, height: 0)
+        profilePicture.layer.shadowRadius = 15
+        profilePicture.layer.shadowOpacity = 0.8
+    }
+    
+    // MARK: Navigation Controller
+    private func navigationControllerStyleSheet() {
+        self.navigationController?.navigationBarHidden = false
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.barTintColor = GWMColorYellow
+        self.navigationController?.navigationBar.topItem?.title = "Profile"
+        
+        let navBarLineView = UIView(frame: CGRectMake(0,
+            CGRectGetHeight((navigationController?.navigationBar.frame)!),
+            CGRectGetWidth((self.navigationController?.navigationBar.frame)!),
+            1))
+        
+        navBarLineView.backgroundColor = GWMColorYellow
+        
+        self.navigationController?.navigationBar.addSubview(navBarLineView)
+        self.navigationController?.navigationBar.tintColor = GWMColorPurple
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : GWMColorPurple]
+        self.tabBarController?.tabBar.backgroundColor = GWMColorYellow
+        self.tabBarController?.tabBar.tintColor = GWMColorPurple
+    }
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -93,44 +132,6 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             profilePicture.setTitle("", forState: .Normal)
             DatabaseHelper.sharedInstance.commitTransaction()
         }
-        
-    }
-    
-    private func resizeToAspectFit(viewSize: CGSize, bounds: CGRect, sourceImage: UIImage) -> UIImage {
-        UIGraphicsBeginImageContext(viewSize)
-        sourceImage.drawInRect(bounds)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image
-    }
-    
-    private func profilePictureStyleSheet() {
-        profilePicture.layer.borderWidth = 3
-        profilePicture.backgroundColor = GWMColorPurple
-        profilePicture.layer.shadowColor = UIColor.blackColor().CGColor
-        profilePicture.layer.shadowOffset = CGSize(width: 0, height: 0)
-        profilePicture.layer.shadowRadius = 15
-        profilePicture.layer.shadowOpacity = 0.8
-    }
-    
-    private func navigationControllerStyleSheet() {
-        self.navigationController?.navigationBarHidden = false
-        self.navigationController?.navigationBar.translucent = false
-        self.navigationController?.navigationBar.barTintColor = GWMColorYellow
-        self.navigationController?.navigationBar.topItem?.title = "Profile"
-        
-        let navBarLineView = UIView(frame: CGRectMake(0,
-            CGRectGetHeight((navigationController?.navigationBar.frame)!),
-            CGRectGetWidth((self.navigationController?.navigationBar.frame)!),
-            1))
-        
-        navBarLineView.backgroundColor = GWMColorYellow
-        
-        self.navigationController?.navigationBar.addSubview(navBarLineView)
-        self.navigationController?.navigationBar.tintColor = GWMColorPurple
-        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : GWMColorPurple]
-        self.tabBarController?.tabBar.backgroundColor = GWMColorYellow
-        self.tabBarController?.tabBar.tintColor = GWMColorPurple
     }
 
     override func didReceiveMemoryWarning() {
