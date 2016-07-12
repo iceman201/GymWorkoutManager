9f73eb7dec1239b3d87e14cf55abcd895551e90d
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2016-07-13 01:33:34 +1200

    update

--- GymWorkoutManager/Model/CommonUtils.swift
+++ GymWorkoutManager/Model/CommonUtils.swift
@@ -28,3 +28,15 @@ func timeToSecond(time: String) -> Int {
     }
     return m * 60 + s
 }
+
+extension NSDate
+{
+    convenience
+    init(dateString:String) {
+        let dateStringFormatter = NSDateFormatter()
+        dateStringFormatter.dateFormat = "yyyy-MM-dd"
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        let d = dateStringFormatter.dateFromString(dateString)!
+        self.init(timeInterval:0, sinceDate:d)
+    }
+}
